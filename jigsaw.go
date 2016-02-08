743c416a1bf782d3d4230e6c373ab91c73ca4bd2
craig brookes <craigbrookes@gmail.com>
2016-02-08 21:30:48 +0000

    improvements to piece cutting

--- jigsaw.go
+++ jigsaw.go
@@ -7,8 +7,6 @@ import (
 	"math"
 
 	"image/color"
-	"encoding/json"
-	"io/ioutil"
 )
 
 type Jigsaw struct {
@@ -18,20 +16,19 @@ type Jigsaw struct {
 	Bounds image.Rectangle
 }
 
-type pieceJoint struct {
+type PieceJoint struct {
 	External bool
-	Side int
+	Side     int
 }
 
-
 type Piece struct {
-	Height			int
-	Width			int
-	Joints           []pieceJoint
+	Height           int
+	Width            int
+	Joints           []PieceJoint
 	Points           []image.Point
 	IsCorner         bool
 	IsEdge           bool
-	IsCenter		 bool
+	IsCenter         bool
 	Name             string
 	Path             string
 	RightPieceIndex  int
@@ -41,49 +38,50 @@ type Piece struct {
 	Index            int
 	Row              int
 	Board            image.Rectangle
-	Bounds			 image.Rectangle
-	Image 			 image.Image
+	Bounds           image.Rectangle
+	Image            image.Image
 }
 
-func (p * Piece)TopRow()bool {
-	for _,p := range p.Points{
-		if p.Y == 0{
-			return true;
+func (p *Piece) TopRow() bool {
+	for _, p := range p.Points {
+		if p.Y == 0 {
+			return true
 		}
 	}
 	return false
 }
-func (p * Piece)BottomRow()bool  {
-	for _,pt := range p.Points{
-		if pt.Y == p.Board.Max.Y{
-			return true;
+func (p *Piece) BottomRow() bool {
+	for _, pt := range p.Points {
+		if pt.Y == p.Board.Max.Y {
+			return true
 		}
 	}
 	return false
 }
 
-func (p *Piece)FarRightVerticalRow()bool  {
-	for _,pt := range p.Points{
-		if pt.X == p.Board.Max.X{
-			return true;
+func (p *Piece) FarRightVerticalRow() bool {
+	for _, pt := range p.Points {
+		if pt.X == p.Board.Max.X {
+			return true
 		}
 	}
 	return false
 }
-func (p *Piece)FarLeftVerticalRow()bool  {
-	for _,pt := range p.Points{
-		if pt.X == p.Board.Min.X{
-			return true;
+func (p *Piece) FarLeftVerticalRow() bool {
+	for _, pt := range p.Points {
+		if pt.X == p.Board.Min.X {
+			return true
 		}
 	}
 	return false
 }
 
 type circle struct {
-	p     image.Point
-	r     int
-	image image.Image
-	inverse  bool
+	P           image.Point
+	R           int
+	image       image.Image
+	inverse     bool
+	InnerBounds image.Rectangle
 }
 
 func (c *circle) ColorModel() color.Model {
@@ -96,53 +94,53 @@ func (c *circle) Bounds() image.Rectangle {
 
 //TODO switch transparent for when the circle is external joint
 func (c *circle) At(x, y int) color.Color {
-	innerBounds := image.Rect(c.image.Bounds().Min.X,c.p.Y,c.image.Bounds().Max.X,c.image.Bounds().Max.Y)
 	//c.p.X is the x point for the center of the circle
 	//c.p.Y is the y point for the center of the circle
-	xx, yy, rr := float64(x-c.p.X)+0.5, float64(y-c.p.Y)+0.5, float64(c.r)
+	xx, yy, rr := float64(x-c.P.X)+0.5, float64(y-c.P.Y)+0.5, float64(c.R)
 	if xx*xx+yy*yy < rr*rr { //the point of ref by x,y is within the diameter of the circle
-		if c.inverse{
+		if c.inverse {
 			return c.image.At(x, y)
-		}else {
+		} else {
 			return color.Transparent
 		}
 	}
-	if c.inverse{
-		if y < innerBounds.Min.Y{
+	if c.inverse {
+		if y < c.InnerBounds.Min.Y || x < c.InnerBounds.Min.X || y > c.InnerBounds.Max.Y || x > c.InnerBounds.Max.X {
 
-			return c.image.At(x,y)
-		}else{
+			return c.image.At(x, y)
+		} else {
 			return color.Transparent
 		}
 
-	}else{
+	} else {
 		return c.image.At(x, y)
 	}
 
 }
 
 type JigsawBuilder struct {
-	PieceCutter		PieceCutter
-	numPieces       int
-	numRows         int
-	numPiecesPerRow int
+	PieceCutter     PieceCutter
+	PieceMarker     PieceMarker
+	NumPieces       int
+	NumRows         int
+	NumPiecesPerRow int
 	baseImage       image.Image
 }
 
 //todo break up
-func (jb *JigsawBuilder) buildPieces() ([]*Piece, error) {
+func (jb *JigsawBuilder) BuildPieces() ([]*Piece, error) {
 
-	piecesPerLine := jb.numPieces / jb.numRows
-	jb.numPiecesPerRow = piecesPerLine
+	piecesPerLine := jb.NumPieces / jb.NumRows
+	jb.NumPiecesPerRow = piecesPerLine
 	rect := jb.baseImage.Bounds()
-	pieceHeight := rect.Max.Y / jb.numRows
+	pieceHeight := rect.Max.Y / jb.NumRows
 	pieceWidth := rect.Max.X / piecesPerLine
 	pieces := make([]*Piece, 0)
 
 	isCorner := func(points []image.Point) bool {
 		var corner = false
 		for _, p := range points {
-			if ((p.X / pieceWidth) == jb.numPiecesPerRow || p.X == 0) && (p.Y / pieceHeight == jb.numRows || p.Y == 0) {
+			if ((p.X/pieceWidth) == jb.NumPiecesPerRow || p.X == 0) && (p.Y/pieceHeight == jb.NumRows || p.Y == 0) {
 				corner = true
 			}
 		}
@@ -154,110 +152,58 @@ func (jb *JigsawBuilder) buildPieces() ([]*Piece, error) {
 			return true
 		}
 		for _, p := range points {
-			if p.Y == 0 || p.Y / pieceHeight == jb.numRows {
+			if p.Y == 0 || p.Y/pieceHeight == jb.NumRows {
 				return true
-			} else if p.X == 0 || p.X / pieceWidth == jb.numPiecesPerRow {
+			} else if p.X == 0 || p.X/pieceWidth == jb.NumPiecesPerRow {
 				return true
 			}
 		}
 		return false
 	}
 
-	isCenter := func(points []image.Point) bool{
-		return (! isCorner(points) && ! isEdge(points))
+	isCenter := func(points []image.Point) bool {
+		return (!isCorner(points) && !isEdge(points))
 	}
 
-//	setNeighbours := func(piece *Piece) {
-//		if piece.IsCorner {
-//			if piece.Index%jb.numPiecesPerRow == 0 && piece.Index == jb.numPieces {
-//				//bottom right
-//				piece.LeftPieceIndex = piece.Index - 1
-//				piece.TopPieceIndex = piece.Index - jb.numPiecesPerRow
-//			} else if piece.Index == jb.numPiecesPerRow {
-//				//top right
-//				piece.LeftPieceIndex = piece.Index - 1
-//				piece.BottomPieceIndex = piece.Index + jb.numPiecesPerRow
-//			} else if piece.Index == 1 {
-//				//top left
-//				piece.RightPieceIndex = piece.Index + 1
-//				piece.BottomPieceIndex = piece.Index + jb.numPiecesPerRow
-//			} else {
-//				//bottom left
-//				piece.RightPieceIndex = piece.Index + 1
-//				piece.TopPieceIndex = piece.Index - jb.numPiecesPerRow
-//			}
-//		} else if piece.IsEdge {
-//			if piece.Index%jb.numPiecesPerRow == 0 {
-//				//right side
-//				piece.LeftPieceIndex = piece.Index - 1
-//				piece.TopPieceIndex = piece.Index - jb.numPiecesPerRow
-//				piece.BottomPieceIndex = piece.Index + jb.numPiecesPerRow
-//			} else if piece.Row == 0 {
-//				//top
-//				piece.RightPieceIndex = piece.Index + 1
-//				piece.LeftPieceIndex = piece.Index - 1
-//				piece.BottomPieceIndex = piece.Index + jb.numPiecesPerRow
-//			} else if piece.Row == jb.numRows {
-//				//bottom
-//				piece.RightPieceIndex = piece.Index + 1
-//				piece.LeftPieceIndex = piece.Index - 1
-//				piece.TopPieceIndex = piece.Index - jb.numPiecesPerRow
-//			} else {
-//				//left
-//				piece.RightPieceIndex = piece.Index + 1
-//				piece.TopPieceIndex = piece.Index - jb.numPiecesPerRow
-//				piece.BottomPieceIndex = piece.Index + jb.numPiecesPerRow
-//			}
-//		} else {
-//			//middle piece
-//			piece.RightPieceIndex = piece.Index +1
-//			piece.LeftPieceIndex  = piece.Index -1
-//			piece.TopPieceIndex = piece.Index - jb.numPiecesPerRow
-//			piece.BottomPieceIndex = piece.Index + jb.numPiecesPerRow
-//		}
-//
-//	}
 	//use type
-	X := func (x, pieceWidth int) (int,int){
+	X := func(x, pieceWidth int) (int, int) {
 		rightX := x + pieceWidth
-		return x,rightX
+		return x, rightX
 	}
 
-	Y := func (y, pieceHeight int)(int,int){
+	Y := func(y, pieceHeight int) (int, int) {
 		topY := y + pieceHeight
 
-		return y,topY
+		return y, topY
 	}
 
 	pieceNum := 1
 	//todo make concurrent
-	for i := 0; i < jb.numRows; i++ {
+	for i := 0; i < jb.NumRows; i++ {
 		currentPosY := i * pieceHeight
-		bottomY,topY:= Y(currentPosY,pieceHeight)
+		bottomY, topY := Y(currentPosY, pieceHeight)
 		for j := 0; j < piecesPerLine; j++ {
 			currentPosX := j * pieceWidth
-			leftX,rightX := X(currentPosX,pieceWidth)
+			leftX, rightX := X(currentPosX, pieceWidth)
 			points := []image.Point{image.Pt(leftX, bottomY), image.Pt(rightX, bottomY), image.Pt(leftX, topY), image.Pt(rightX, topY)}
 			corner := isCorner(points)
-			fmt.Println("** is Corner ", corner, piecesPerLine, pieceWidth)
-			p := &Piece{Height: pieceHeight, Width: pieceWidth,Points: points, IsCorner: corner, IsEdge: isEdge(points), Name: fmt.Sprintf("piece%d", pieceNum), Index: pieceNum, Row: i,Board:jb.baseImage.Bounds(), IsCenter:isCenter(points),  Joints:[]pieceJoint{{External:false,Side:1},{External:true,Side:2}}}
+			p := &Piece{Height: pieceHeight, Width: pieceWidth, Points: points, IsCorner: corner, IsEdge: isEdge(points), Name: fmt.Sprintf("piece%d", pieceNum), Index: pieceNum, Row: i, Board: jb.baseImage.Bounds(), IsCenter: isCenter(points), Joints: nil}
+			p.Bounds = image.Rect(points[0].X, points[0].Y, points[3].X, points[3].Y)
 			pieces = append(pieces, p)
 			pieceNum++
 		}
 	}
-	data,_ := json.MarshalIndent(pieces,"", " ")
-	ioutil.WriteFile("./out/out.json",data,777)
 	return pieces, nil
 
 }
 
 func (jb *JigsawBuilder) buildRows() error {
 	//we are only interested in whole numbers
-	sqr := int(math.Sqrt(float64(jb.numPieces)))
-	if jb.numPieces%sqr != 0 {
+	sqr := int(math.Sqrt(float64(jb.NumPieces)))
+	if jb.NumPieces%sqr != 0 {
 		return errors.New("num pieces should have whole number square root. ie 4, 8, 12, 15")
 	}
-	jb.numRows = sqr
+	jb.NumRows = sqr
 	return nil
 }
 
@@ -268,12 +214,12 @@ func (jb *JigsawBuilder) Build() (Jigsaw, error) {
 	if err != nil {
 		return jig, err
 	}
-	pieces, err := jb.buildPieces()
+	pieces, err := jb.BuildPieces()
 	if err != nil {
 		return jig, err
 	}
-
-	pieces, err = jb.PieceCutter.CutPieces(jb.baseImage,pieces)
+	pieces = jb.PieceMarker.MarkPieces(pieces, jb.NumPiecesPerRow, jb.NumRows)
+	pieces, err = jb.PieceCutter.CutPieces(jb.baseImage, pieces)
 	if err != nil {
 		return jig, err
 	}
@@ -282,16 +228,13 @@ func (jb *JigsawBuilder) Build() (Jigsaw, error) {
 }
 
 func NewJigsawBuilder(img image.Image, numPieces int) *JigsawBuilder {
-	return &JigsawBuilder{numPieces: numPieces, baseImage: img, PieceCutter: JigsawPieceCutter{}}
+	return &JigsawBuilder{NumPieces: numPieces, baseImage: img, PieceCutter: JigsawPieceCutter{}, PieceMarker: JigsawPieceMarker{}}
 }
 func NewJigsawBuilderWithPieceCutter(img image.Image, numPieces int, cutter JigsawPieceCutter) *JigsawBuilder {
-	return &JigsawBuilder{numPieces: numPieces, baseImage: img, PieceCutter: cutter}
+	return &JigsawBuilder{NumPieces: numPieces, baseImage: img, PieceCutter: cutter, PieceMarker: JigsawPieceMarker{}}
 }
-
 
 type CutType struct {
 	Internal bool
-	Edge int //0-4
+	Edge     int //0-4
 }
-
-
--- piece_cutter.go
+++ piece_cutter.go
@@ -1,134 +1,274 @@
 package jigsaw
+
 import (
-"image"
-"errors"
+	"errors"
+	"fmt"
 	"github.com/disintegration/imaging"
 	"github.com/llgcode/draw2d/draw2dimg"
-"image/draw"
-	"fmt"
+	"golang.org/x/image/draw"
+	"image"
 )
 
-const PERCENTAGE = 11.0
+const PERCENTAGE = 10.0
 
 type PieceCutter interface {
-	CutPieces(from image.Image, pieces []*Piece)([]*Piece,error)
+	CutPieces(from image.Image, pieces []*Piece) ([]*Piece, error)
 }
 
-func Percentage(total int, percentage float64)int{
-	return int((percentage /  100.0) * float64(total))
+const TOP_SIDE = 0
+const RIGHT_SIDE = 1
+const BOTTOM_SIDE = 2
+const LEFT_SIDE = 3
+
+type PieceMarker interface {
+	MarkPieces([]*Piece, int, int) []*Piece
 }
 
-type JigsawPieceCutter struct {}
+func Percentage(piece *Piece, percentage float64) int {
+	if piece.Height > piece.Width{
+		return int((percentage / 100.0) * float64(piece.Height))
+	}
+	return int((percentage / 100.0) * float64(piece.Width))
+}
+
+type JigsawPieceCutter struct{}
+type JigsawPieceMarker struct{}
+
+func (JigsawPieceMarker) MarkPieces(pieces []*Piece, piecesPerRow, numRows int) []*Piece {
+	//start at 0 move across
+	retPieces := make([]*Piece, 0)
+	for i := 0; i < len(pieces); i++ {
+		p := pieces[i]
+		if p.IsCorner {
+			if (i+1)/piecesPerRow == 1 || (i+1)/piecesPerRow == numRows {
+				//top right or bottom right
+				p.Joints = []PieceJoint{{
+					External: false,
+					Side:     LEFT_SIDE,
+				}}
+				if (i+1)/piecesPerRow == numRows {
+					p.Joints = append(p.Joints, PieceJoint{
+						External: true,
+						Side:     TOP_SIDE,
+					})
+				} else {
+					p.Joints = append(p.Joints, PieceJoint{
+						External: false,
+						Side:     BOTTOM_SIDE,
+					})
+				}
+			} else {
+
+				p.Joints = []PieceJoint{{
+					External: true,
+					Side:     RIGHT_SIDE,
+				}}
+				if i == 0 {
+					p.Joints = append(p.Joints, PieceJoint{
+						External: false,
+						Side:     BOTTOM_SIDE,
+					})
+				} else {
+					p.Joints = append(p.Joints, PieceJoint{
+						External: true,
+						Side:     TOP_SIDE,
+					})
+				}
+			}
+		}
+
+		retPieces = append(retPieces, p)
+	}
+	return retPieces
+}
+
 //cuts a rectangular piece with additional space added for any external joints
-func (JigsawPieceCutter)cutPiece(from image.Image, piece *Piece)(*Piece,error)  {
+func (JigsawPieceCutter) cutPiece(from image.Image, piece *Piece) (*Piece, error) {
 	//cut the piece 20%larger on the sides that have external joints
 
-	for _,joint:= range piece.Joints{
-		if joint.External{
-			if joint.Side == 0{
+	for _, joint := range piece.Joints {
+		if joint.External {
+			if joint.Side == TOP_SIDE {
 				// 0 is top side  (positive)
 				//grow point[0].Y unless it already at max
-				piece.Points[0].Y-= Percentage(piece.Height,PERCENTAGE)
-			}else if joint.Side == 1{
+				piece.Points[0].Y -= Percentage(piece, PERCENTAGE)
+			} else if joint.Side == RIGHT_SIDE {
 				//1 is right side
 				//grow point[3].X (positive)unless it already at max
-				piece.Points[3].X+= Percentage(piece.Width, PERCENTAGE)
-			}else if joint.Side == 2{
+				piece.Points[3].X += Percentage(piece, PERCENTAGE)
+			} else if joint.Side == BOTTOM_SIDE {
 				//2 is bottom side
 				//grow point[3].Y (by a negative value)	 unless it already at min
-				piece.Points[3].Y+= Percentage(piece.Height, PERCENTAGE)
-			}else if joint.Side == 3{
+				fmt.Println("Y is ", piece.Points[3].Y)
+				piece.Points[3].Y += Percentage(piece, PERCENTAGE)
+				fmt.Println("Y is ", piece.Points[3].Y)
+			} else if joint.Side == LEFT_SIDE {
 				//3 is left side
 				//grow point[0].X (negative)
-				piece.Points[0].X-= Percentage(piece.Width, PERCENTAGE)
+				piece.Points[0].X -= Percentage(piece, PERCENTAGE)
 			}
 		}
 		//non external joints are cut into the existing piece
 	}
 	piece.Bounds = image.Rect(piece.Points[0].X, piece.Points[0].Y, piece.Points[3].X, piece.Points[3].Y)
-	rectCropImg := imaging.Crop(from, piece.Bounds )
+	//fmt.Println("BOUNDS CREATED as ", piece.Bounds)
+	rectCropImg := imaging.Crop(from, piece.Bounds)
 	piece.Image = rectCropImg
-	path := "./out/" + piece.Name + ".jpg"
+	path := "./out/" + piece.Name + ".png"
 	// save cropped image
 	err := imaging.Save(rectCropImg, path)
 	piece.Path = path
 	return piece, err
 }
 
+func variableRadius(basedOn *Piece) int {
+	return Percentage(basedOn, PERCENTAGE)
+}
+
+func hasExternalJoint(piece *Piece, side int) bool {
+	for _, j := range piece.Joints {
+		if j.Side == side {
+			return j.External
+		}
+	}
+	return false
+}
+
+type JointCutter struct {
+	Piece *Piece
+}
+
+func (jc JointCutter) cutInternal(joint PieceJoint, img image.Image) (image.Image, error) {
+	var radius, x, y int
+
+	if joint.Side == TOP_SIDE {
+		radius = Percentage(jc.Piece, PERCENTAGE)
+		x, y = img.Bounds().Max.X/2, img.Bounds().Min.Y
+	} else if joint.Side == RIGHT_SIDE {
+		radius = Percentage(jc.Piece, PERCENTAGE)
+		x, y = img.Bounds().Max.X, img.Bounds().Max.Y/2
+	} else if joint.Side == BOTTOM_SIDE {
+		radius = Percentage(jc.Piece, PERCENTAGE)
+		x, y = img.Bounds().Max.X/2, img.Bounds().Max.Y
+	} else if joint.Side == LEFT_SIDE {
+		radius = Percentage(jc.Piece, PERCENTAGE)
+		percentInc := 0.0
+		if hasExternalJoint(jc.Piece, 0) {
+			percentInc += PERCENTAGE
+		}
+		if hasExternalJoint(jc.Piece, 2) {
+			percentInc += PERCENTAGE
+		}
+		x, y = img.Bounds().Min.X, (img.Bounds().Max.Y+Percentage(jc.Piece, percentInc))/2
+	}
+	imageContext := image.NewRGBA(img.Bounds())
+	circleImg := &circle{image.Pt(x, y), radius, img, false, image.Rectangle{}}
+	draw.Draw(imageContext, img.Bounds(), circleImg, img.Bounds().Min, draw.Src)
+	draw2dimg.SaveToPngFile("./out/"+jc.Piece.Name+".png", imageContext)
+	return imageContext, nil
+}
+
+func (jc JointCutter) cutExternal(joint PieceJoint, from image.Image) (image.Image, error) {
+
+	var x, y int
+	var piece = jc.Piece
+	var imageContext = image.NewRGBA(from.Bounds())
+	var circleImg *circle
+	var percentInc = 0.0
+
+	if joint.Side == RIGHT_SIDE || joint.Side == LEFT_SIDE {
+		if hasExternalJoint(jc.Piece,0) && hasExternalJoint(jc.Piece,2){
+			percentInc = 0.0
+		}else if hasExternalJoint(jc.Piece, 0) {
+			percentInc += PERCENTAGE
+		}else if hasExternalJoint(jc.Piece, 2) {
+			percentInc += PERCENTAGE
+		}
+	} else if joint.Side == BOTTOM_SIDE || joint.Side == TOP_SIDE {
+		if hasExternalJoint(jc.Piece, 3) && hasExternalJoint(jc.Piece,1){
+			percentInc = 0.0
+		}else if hasExternalJoint(jc.Piece, 3) {
+			percentInc += PERCENTAGE
+		}else if hasExternalJoint(jc.Piece, 1) {
+			percentInc += PERCENTAGE
+		}
+	}
+	if joint.Side == RIGHT_SIDE {
+		//move back Percentage amount
+		x, y = (from.Bounds().Max.X - Percentage(piece, PERCENTAGE)), (from.Bounds().Max.Y  + Percentage(piece,percentInc)) / 2 //half way down the right hand side
+		point := image.Pt(x, y)
+		circleImg = &circle{point, variableRadius(piece), from, true, image.Rect(point.X, from.Bounds().Max.Y, from.Bounds().Max.X, from.Bounds().Min.Y)}
+	}
+	//left
+	if joint.Side == LEFT_SIDE {
+
+		x, y = (from.Bounds().Min.X + Percentage(piece, PERCENTAGE)), (from.Bounds().Max.Y + Percentage(piece,percentInc)) /2 //half way down the left side
+		point := image.Pt(x, y)
+		circleImg = &circle{point, variableRadius(piece), from, true, image.Rect(point.X, from.Bounds().Min.Y, from.Bounds().Min.X, from.Bounds().Max.Y)}
+
+	}
+	//top
+	if joint.Side == TOP_SIDE {
+		x, y = from.Bounds().Max.X/2, from.Bounds().Min.Y+Percentage(piece, PERCENTAGE) //half way across top line
+		point := image.Pt(x, y)                                                                //where the circle is centered around
+		circleImg = &circle{point, variableRadius(piece), from, true, image.Rect(from.Bounds().Max.X, y, from.Bounds().Min.X, from.Bounds().Min.Y)}
+	}
+	//bottom
+	if joint.Side == BOTTOM_SIDE {
+		x, y = from.Bounds().Max.X/2, (from.Bounds().Max.Y - Percentage(piece, PERCENTAGE)) //half way across bottom line
+		point := image.Pt(x, y)
+		circleImg = &circle{point, variableRadius(piece), from, true, image.Rect(from.Bounds().Min.X, point.Y, from.Bounds().Max.X, from.Bounds().Max.Y)}
+
+	}
+	draw.Draw(imageContext, from.Bounds(), circleImg, from.Bounds().Min, draw.Src)
+	return imageContext, nil
+
+}
+
 //shapes the rectangular piece removing and add joint pieces
-func (JigsawPieceCutter)shapePiece(piece *Piece)(*Piece, error)  {
+func (JigsawPieceCutter) ShapePiece(piece *Piece) (*Piece, error) {
 	//fill a semi circle with transparency
 	//the piece is 20% larger on each side add any cuts then crop down sides without external piece
-	var circleImg *circle
-
-	var radius int = Percentage(piece.Bounds.Max.Y,PERCENTAGE /2)
-	var imageContext *image.RGBA
-	var x,y int
-	img := piece.Image
-	bounds := img.Bounds()
-
-	for _,cut := range piece.Joints{
-		if ! cut.External{
-
-
-			if cut.Side == 0{
-				x,y = bounds.Max.X/2, bounds.Max.Y
-			}else if cut.Side == 1{
-				x,y = bounds.Max.X, bounds.Max.Y / 2
-			}else if cut.Side == 2{
-				x,y = bounds.Max.X /2 , bounds.Min.Y
-			}else if cut.Side == 3{
-				x,y = bounds.Min.X , bounds.Max.Y /2
+	jointCutter := JointCutter{Piece: piece}
+	var img = piece.Image
+	var err error
+	for _, joint := range piece.Joints {
+		if !joint.External {
+			img, err = jointCutter.cutInternal(joint, img)
+			if err != nil {
+				return nil, err
 			}
-			imageContext = image.NewRGBA(bounds)
-			circleImg = &circle{image.Pt(x,y), radius, img, false}
-			draw.Draw(imageContext, img.Bounds(), circleImg, img.Bounds().Min, draw.Src)
-			fmt.Println("saving png to file")
-			draw2dimg.SaveToPngFile("./test.png", imageContext)
-			img = imageContext
-		}else{
-			if cut.Side == 2{
-				x,y = bounds.Max.X / 2, bounds.Max.Y //half way across bottom line
-				//need to move back 20 %
-				y = y - Percentage(y,PERCENTAGE)
-				//draw a circle filled with the background
-				imageContext = image.NewRGBA(bounds)
-				circleImg = &circle{image.Pt(x,y), radius, img, true}
-				draw.Draw(imageContext, bounds, circleImg, bounds.Min, draw.Src)
-				draw2dimg.SaveToPngFile("./testout.png", imageContext)
-				img = imageContext
-				//draw rectangle that cuts across
-
-			}
+		} else {
+			img, err = jointCutter.cutExternal(joint, img)
 
 		}
 	}
+	draw2dimg.SaveToPngFile("./out/"+piece.Name+".png", img)
 
-
-	return piece,nil
+	return piece, nil
 }
 
-func (jpc JigsawPieceCutter)shapePieces(pieces []*Piece)([]*Piece, error){
-	shapedPieces := make([]*Piece,len(pieces))
-	for i,piece := range pieces{
-		shaped,err :=jpc.shapePiece(piece)
-		if nil != err{
-			return nil,err
+func (jpc JigsawPieceCutter) ShapePieces(pieces []*Piece) ([]*Piece, error) {
+	shapedPieces := make([]*Piece, len(pieces))
+	for i, piece := range pieces {
+		shaped, err := jpc.ShapePiece(piece)
+		if nil != err {
+			return nil, err
 		}
 		shapedPieces[i] = shaped
 	}
-	return shapedPieces,nil
+	return shapedPieces, nil
 }
 
-func (jpc JigsawPieceCutter)CutPieces(from image.Image, pieces []*Piece)([]*Piece,error) {
-	var cutPieces = make([]*Piece,len(pieces))
-	for index,p := range pieces{
-		cutPiece,err := jpc.cutPiece(from,p)
-		if err != nil{
+func (jpc JigsawPieceCutter) CutPieces(from image.Image, pieces []*Piece) ([]*Piece, error) {
+	var cutPieces = make([]*Piece, len(pieces))
+	for index, p := range pieces {
+		cutPiece, err := jpc.cutPiece(from, p)
+		if err != nil {
 			return nil, errors.New("failed to cut piece " + err.Error())
 		}
 		cutPieces[index] = cutPiece
 	}
-	return jpc.shapePieces(cutPieces)
+	return jpc.ShapePieces(cutPieces)
 
 }
